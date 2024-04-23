565e0b90fcd6dc6c72f77bf04f9f6e3e009e10d8
MattHill-2025 <matt.hill.1500@gmail.com>
2024-04-23 11:28:20 -0400

    Update proj6.cpp

--- proj6.cpp
+++ proj6.cpp
@@ -6,6 +6,36 @@ using namespace std;
 
 #define MAX_VALUE = 30
 
+void bubbleSort(int arr[], int n){
+    //int i, j;
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+}
+
+void selectionSort(int arr[], int n){
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
+    }
+}
+
+
 void fill_arr(int fthir[], int sthir[]){
     random_device rd;
     mt19937 gen(rd());
@@ -20,18 +50,31 @@ void fill_arr(int fthir[], int sthir[]){
     }
 }
 
+void printArray(int arr[], int size){
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(){
     int fthir [30];
     int sthir [30];
+    int count = 15;
 
     fill_arr(fthir, sthir);
     
-    for(int i = 0; i < 15, i++;){
-        cout << fthir[i] << endl;
-    }
+    
 
-    for(int i = 0; i < 15, i++;){
-        cout << sthir[i] << endl;
-    }
+    printArray(fthir, count);
+    bubbleSort(fthir, count);
+    printArray(fthir, count);
+
+    cout << endl;
+
+    printArray(sthir, count);
+    selectionSort(sthir, count);
+
+    
 
 }
